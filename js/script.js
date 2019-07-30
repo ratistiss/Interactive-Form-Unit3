7c339b902da603a6dc3c81ab4e61a59b95478530
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-07-30 11:57:38 -0400

    Update 1.3
    Payment Info shows related to which payment option you choose

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ $('#color :not(:first)').hide();
 
 $("#design").change(function(){
      if($(this).val() == 'js puns'){
-        //  $("#color :nth-child(1)").hide();
          $("#color").children().hide();
          $("#color :nth-child(2)").show(); 
          $("#color :nth-child(3)").show(); 
@@ -18,4 +17,54 @@ $("#design").change(function(){
     }});
 
 
-    
+let totalActivityCost = $('.activities').append('div');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('#payment :nth-child(1)').hide();
+$('#credit-card').hide();
+$('div p').eq(0).hide();
+$('div p').eq(1).hide();
+
+$('#payment').change(function () {
+    if ($(this).val() == 'credit card') {
+        $("#credit-card").show();
+        $('div p').eq(0).hide();
+        $('div p').eq(1).hide();
+    } else if ($(this).val() == 'paypal') {
+        $('div p').eq(0).show();
+        $('#credit-card').hide();
+        $('div p').eq(1).hide();
+    } else if ($(this).val() == 'bitcoin') {
+        $('div p').eq(1).show();
+        $('#credit-card').hide();
+        $('div p').eq(0).hide();
+
+    }});
