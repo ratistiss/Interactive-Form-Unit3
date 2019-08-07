34fccb505dc0cf803a37f1b7eb59a5d69098218c
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-08-06 21:55:26 -0400

    Update 2.1
    name changes, refactor

--- js/script.js
+++ js/script.js
@@ -106,13 +106,13 @@ $('#payment').change(function () {
 
  
 //Name validation with regex
-const nameVal = () => {
-    const isValidName = () => {
+const $name = () => {
+    const $validName = () => {
         const name = $('#name').val();
         const nameCheck = /^[a-zA-Z ]+$/.test(name);
         return nameCheck;
     }
-    if (isValidName() === true) {
+    if ($validName() === true) {
         $('label[for="name"]').css('color', '');
         $('#name').css('border', '');
     } else {
@@ -123,18 +123,18 @@ const nameVal = () => {
 
 //Name field  event listener
 $('#name').on('change', function () {
-    nameVal();
+    $name();
 });
 
 
 //Email validation with regex
-const emailVal = () => {
-    const isValidEmail = () => {
+const $email = () => {
+    const $validEmail = () => {
         const email = $('#mail').val();
         const emailCheck = /^([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)$/i.test(email);
         return emailCheck;
     }
-    if (isValidEmail()) {
+    if ($validEmail()) {
         $('label[for="mail"]').css('color', '');
         $('#mail').css('border', '');
     } else {
@@ -145,11 +145,11 @@ const emailVal = () => {
 
 //Email field event listener
 $('#mail').on('change', function () {
-    emailVal();
+    $email();
 });
 
 //Activities checked checker function
-const checked = () => {
+const $checked = () => {
     const activityCount = $('.activities input:checkbox:checked').length;
     const activityLegend = $('.activities legend');
     if (activityCount > 0) {
@@ -166,7 +166,7 @@ $('.activities').on('change', function () {
 });
 
 //CC number regex for Mastercard's, Visa's, Amex, Discover, and Diner's Club. Regex was taken from http://www.informit.com/articles/article.aspx?p=1223879&seqNum=12
-const isValidCC = () => {
+const $validCC = () => {
     const ccNum = parseInt($('#cc-num').val());
     const card = /^(5[1-5]\d{14})|(4\d{12}(\d{3})?)|(3[47]\d{13})|(6011\d{14})|((30[0-5]|36\d|38\d)\d{11})$/.test(ccNum);
     if (card) {
@@ -175,36 +175,36 @@ const isValidCC = () => {
 }
 
 //Zip code regex 5 digits
-const isValidZip = () => {
+const $validZip = () => {
     const zipCode = $('#zip').val();
     const zipCodeCheck = /(^\d{5}$)/.test(zipCode);
     return zipCodeCheck;
 }
 
 //CVV code regex for 3 digits
-const isValidCVV = () => {
+const $validCVV = () => {
     const cvv = $('#cvv').val();
     const cvvCheck = /^[0-9]{3}$/.test(cvv);
     return cvvCheck;
 }
 
 //Credit Card field Validation for number, zip code and cvv
-    const cardVal = () => {
-        if (isValidCC()) {
+    const $card = () => {
+        if ($validCC()) {
          $('label[for="cc-num"]').css('color', '');
          $('#cc-num').css('border', '');
      } else {
          $('label[for="cc-num"]').css('color', 'red');
          $('#cc-num').css('border', '2px solid red');
      }
-        if (isValidZip()) {
+        if ($validZip()) {
          $('label[for="zip"]').css('color', '');
          $('#zip').css('border', '');
      } else {
          $('label[for="zip"]').css('color', 'red');
          $('#zip').css('border', '2px solid red');
      } 
-       if (isValidCVV()) {
+       if ($validCVV()) {
          $('label[for="cvv"]').css('color', '');
          $('#cvv').css('border', '');
      } else {
@@ -216,14 +216,14 @@ const isValidCVV = () => {
 
 //Credit card field event listener for number, zip code and cvv
 $('#cc-num, #zip, #cvv').on('change', function () {
-    cardVal();
+    $card();
 });
 
 //Credit card selected function to return boolean for master validation
-const isValidPayment = () => {
+const $payment = () => {
     const paymentVal = $('#payment :selected').text();
     if (paymentVal === '' || paymentVal === 'Credit Card') {
-        if (isValidCC() && isValidZip() && isValidCVV()) {
+        if ($validCC() && $validZip() && $validCVV()) {
             return true;
         }
         return false;
@@ -232,12 +232,12 @@ const isValidPayment = () => {
 }
 
 //Page validation function
-const masterVal = () => {
-    if (isValidName() && isValidEmail() && checked() > 0) {
+const $page = () => {
+    if ($validName() && $validEmail() && $checked() > 0) {
         if ($('#payment :selected').val() === 'select_method') {
             return false;
             if ($('#payment :selected').val() === 'credit card') {
-                if (isValidPayment()) {
+                if ($payment()) {
                     return true;
                 }
                 return false;
@@ -249,12 +249,12 @@ const masterVal = () => {
 
 //Submit Button 
 $('button').on('click', function (e) {
-    if (masterVal()) {
+    if ($page()) {
     } else {
         e.preventDefault();
-        nameVal();
-        emailVal();
-        checked();
-        cardVal();
+        $name();
+        $email();
+        $checked();
+        $card();
         alert('Please fill out all fields.');
     }});
